d0e5a4f0ae0946ab5fea25e90a0006e9d2ebecd6
developerbola <developerbola08@gmail.com>
2025-07-13 21:55:49 +0500

    Update 0.1.1 - selection background change for all themes and add test code file for repo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # dark-pro-vscode-default
 
+
+## 0.1.1 - fix code selection background for all themes
+
 ## 0.1.0 - Add Dark Pro Github ( Black ) & minor color fix
 
 ## 0.0.3 - edit README.md
---
+++ index.test.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      <h1>My Test Case</h1>
+      <p data-used="true" style={{ color: "#cccccc" }}>
+        for testing color of theme's code sytaxes
+      </p>
+    </div>
+  );
+};
+
+export default App;
